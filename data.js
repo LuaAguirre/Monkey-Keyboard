28c80e6d511aa2776ac77a831c5dc4b7735fbf5d
LuaAguirre <renatoag98@gmail.com>
2024-07-14 04:22:02 -0500

    first-commit

---
+++ data.js
@@ -0,0 +1,15 @@
+export const words = [
+    "apple", "banana", "car", "dog", "elephant", "flower", "guitar", "house", "ice cream", "jungle", 
+    "kite", "lion", "moon", "nest", "ocean", "penguin", "queen", "rainbow", "sun", "tree", "umbrella", 
+    "violin", "watermelon", "xylophone", "yacht", "zebra", "beach", "chocolate", "dolphin", "eagle", 
+    "fire", "garden", "helicopter", "island", "jacket", "koala", "lemon", "mountain", "notebook", 
+    "orange", "piano", "quilt", "rocket", "snake", "tiger", "unicorn", "volcano", "whale", "xylophone", 
+    "yogurt", "zoo", "astronaut", "butterfly", "caterpillar", "dinosaur", "earthquake", "football", 
+    "giraffe", "hamburger", "igloo", "jigsaw", "kiwi", "lighthouse", "mango", "night", "octopus", 
+    "parrot", "quokka", "rabbit", "spaceship", "tornado", "ufo", "vampire", "waffle", "xylophone", 
+    "yeti", "zeppelin", "antelope", "bicycle", "cactus", "dandelion", "ear", "flamingo", "gorilla", 
+    "hotdog", "iceberg", "jellyfish", "kangaroo", "leopard", "mushroom", "ninja", "ostrich", "palm", 
+    "quail", "rain", "squirrel", "trampoline", "unicorn", "volleyball", "waterfall", "xylophone", 
+    "yogurt", "zeppelin"
+  ];
+  
